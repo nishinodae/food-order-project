a890afc7451a1668fb395ccad615c738a7581602
nishinodae <masyitahsalwa@gmail.com>
2025-11-11 18:43:18 +0800

    formatted document, wrap img onClick function with useCallback

--- food-order-app/src/pages/Admin/components/FilePicker.js
+++ food-order-app/src/pages/Admin/components/FilePicker.js
@@ -1,17 +1,24 @@
 import { Dialog, DialogTitle, Grid } from '@mui/material';
 import { useFoodContext } from '../../../context/FoodMngrContext';
+import { useCallback } from 'react';
 
 const FilePicker = ({ onClose }) => {
     const { setCurrentImage, foodImages } = useFoodContext();
 
+    const handleChoose = useCallback((url) => {
+        onClose();
+        setCurrentImage(url);
+    }, []);
+
     const renderImageList = foodImages.map((item, index) =>
-    (<img key={index} loading='lazy' src={item.imgURL} alt='' width='100px' height='100px'
-        onClick={() => {
-            onClose();
-            setCurrentImage(item.imgURL);
-        }}
-    />
-    ));
+        <img key={index}
+            loading='lazy'
+            src={item.imgURL}
+            alt={item.imgURL?.split('/').pop() || 'not available'}
+            width='100px' height='100px'
+            onClick={() => { handleChoose(item.imgURL); }}
+        />
+    );
 
     return (<Dialog open onClose={onClose}>
         <DialogTitle variant='body1' sx={{ pl: '16px' }}>Choose image:</DialogTitle>
