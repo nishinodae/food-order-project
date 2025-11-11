ad9aafaef6506f73674fe7652b8922788e2cc866
nishinodae <masyitahsalwa@gmail.com>
2025-11-11 18:34:05 +0800

    extracted out the compressImage()

--- food-order-app/src/pages/Admin/components/AddImage.js
+++ food-order-app/src/pages/Admin/components/AddImage.js
@@ -5,6 +5,7 @@ import LaptopIcon from '@mui/icons-material/Laptop';
 import { useEffect, useRef, useState } from 'react';
 import { useFoodContext } from '../../../context/FoodMngrContext';
 import FilePicker from './FilePicker';
+import compressImage from '../../../utils/compressImage';
 
 const AddImage = () => {
     const { currentImage, setCurrentImage } = useFoodContext();
@@ -30,45 +31,6 @@ const AddImage = () => {
 
     }, [currentImage]);
 
-    const compress = async (file) => {
-        const img = new Image();
-        const reader = new FileReader();
-
-        reader.onload = (e) => {
-            img.src = e.target.result;
-        };
-
-        reader.readAsDataURL(file);
-        img.onload = () => {
-            const canvas = document.createElement('canvas');
-            const ctx = canvas.getContext('2d');
-
-            canvas.width = img.width;
-            canvas.height = img.height;
-
-            // Fill the background with white before drawing
-            ctx.fillStyle = '#FFFFFF'; 
-            ctx.fillRect(0, 0, canvas.width, canvas.height);
-            ctx.drawImage(img, 0, 0);
-
-            canvas.toBlob((blob) => {
-                if (blob.size > 10 * 1024 * 1024) {
-                    compress(new File([blob]));
-                } else {
-                    let newfile = new File([blob], `${file.name.replace(/\.[^/.]+$/, '')}-compressed.jpg`, {
-                        type: 'image/jpeg',
-                        lastModified: new Date().getTime()
-                    });
-                    setCurrentImage(newfile);
-                }
-
-            },
-                'image/jpeg', 0.7
-            );
-
-        };
-    };
-
     const handleMenuOpen = (event) => {
         setAnchorEl(event.currentTarget);
     };
@@ -83,16 +45,18 @@ const AddImage = () => {
         const file = e.target.files[0];
         if (file) {
             setLoading(true);
-            //file size should not be larger than 10MB
-            if (file.size > 10 * 1024 * 1024) {
-                await compress(file);
-            }
-            else if (!file.type.startsWith('image/')) {
+            if (!file.type.startsWith('image/')) {
                 alert('Please select an image file');
                 setLoading(false);
                 return;
             } else {
-                setCurrentImage(file);
+                try {
+                    const newFile = await compressImage(file);
+                    setCurrentImage(newFile);
+                }
+                catch (e) {
+                    alert('Something went wrong while compressing the image.');
+                }
             }
         }
     };
@@ -112,8 +76,7 @@ const AddImage = () => {
                 >
                     add image
                 </Button>
-                {loading && <CircularProgress size='12px' />}
-
+                {loading && <CircularProgress size='14px' />}
                 <Menu
                     id='menu-appbar'
                     anchorEl={anchorEl}
@@ -147,7 +110,7 @@ const AddImage = () => {
             </div>
             {currentImage &&
                 <div>
-                    <Typography fontSize='12px'>{imgDisplay[0]}</Typography>
+                    <Typography fontSize='14px'>{imgDisplay[0]}</Typography>
                     <img loading='lazy' src={imgDisplay[1]} alt='' width='100px' height='100px' />
                 </div>
             }
