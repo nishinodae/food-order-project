ecdcb9317a1f0d780c422191dd4b6c8627a0f801
nishinodae <masyitahsalwa@gmail.com>
2025-11-11 18:45:23 +0800

    resized assets, removed lazy loading, changed gradient color

--- food-order-app/src/components/Headline.js
+++ food-order-app/src/components/Headline.js
@@ -1,25 +1,25 @@
 import { Box, Grid, Typography } from '@mui/material';
-import headerImg1 from '../assets/nasi-lemak-illustration.webp';
-import headerImg2 from '../assets/laksa.webp';
-import headerImg3 from '../assets/nasigoreng.webp';
+import headerImg1 from '../assets/nasi-lemak-illustration-resized.webp';
+import headerImg2 from '../assets/laksa-resized.webp';
+import headerImg3 from '../assets/nasigoreng-resized.webp';
 
 const Headline = ({ firstLine, secondLine }) => {
     return (<Grid container sx={{
         position: 'relative', bgcolor: 'primary.main', p: '100px 15px 50px 25px',
-        background: 'radial-gradient(farthest-side at 110% 1%, black, #ff359aff)'
+        background: 'radial-gradient(farthest-side at 110% 1%, #000, #E60071)'
     }}>
         <Box>
             <Typography sx={{ color: 'secondary.main' }} fontSize='20px'>{firstLine}</Typography>
-            <Typography sx={{ color: 'secondary.main' }} fontSize='12px'>{secondLine}</Typography>
+            <Typography sx={{ color: 'secondary.main' }} fontSize='14px'>{secondLine}</Typography>
         </Box>
         <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-            <img src={headerImg1} alt='nasi-lemak' loading='lazy'
+            <img src={headerImg1} alt='nasi-lemak'
                 className='overlay-first'
             ></img>
-            <img src={headerImg2} alt='nasi-lemak' loading='lazy'
+            <img src={headerImg2} alt='nasi-lemak'
                 className='overlay-second'
             ></img>
-            <img src={headerImg3} alt='nasi-lemak' loading='lazy'
+            <img src={headerImg3} alt='nasi-lemak'
                 className='overlay-third'
             ></img>
         </Box>
