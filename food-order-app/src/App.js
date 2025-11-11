e995cc10dcfef28cbef0773136c0f9aebfd707d6
nishinodae <masyitahsalwa@gmail.com>
2025-11-11 12:50:14 +0800

    lazy import Customer and Admin. Use Suspense

--- food-order-app/src/App.js
+++ food-order-app/src/App.js
@@ -1,12 +1,14 @@
-import Admin from './pages/Admin/Admin';
 import { ThemeProvider } from '@emotion/react';
 import { theme } from './utils/theme';
-import Customer from './pages/Customer/Customer';
 import { useAuthContext } from './context/AuthContext';
-import { CssBaseline } from '@mui/material';
+import { CssBaseline, LinearProgress } from '@mui/material';
 import { FoodMngrProvider } from './context/FoodMngrContext';
 import { CartContextProvider } from './context/CartContext';
 import { OrderProvider } from './context/OrderContext';
+import { lazy, Suspense } from 'react';
+
+const Customer = lazy(() => import('./pages/Customer/Customer'));
+const Admin = lazy(() => import('./pages/Admin/Admin'));
 
 function App() {
   const { user } = useAuthContext();
@@ -14,11 +16,13 @@ function App() {
   return (
     <><ThemeProvider theme={theme}>
       <CssBaseline />
-      <FoodMngrProvider>
-        <OrderProvider>
-        {user === 'customer' ? <CartContextProvider><Customer /></CartContextProvider> : <Admin />}
-        </OrderProvider>
-      </FoodMngrProvider>
+      <OrderProvider>
+        <FoodMngrProvider>
+          <Suspense fallback={<LinearProgress/>}>
+          {user === 'customer' ? <CartContextProvider><Customer /></CartContextProvider> : <Admin />}
+          </Suspense>
+        </FoodMngrProvider>
+      </OrderProvider>
     </ThemeProvider>
     </>
   );
